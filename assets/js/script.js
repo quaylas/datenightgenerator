5c45d8b15c7b8be9484435000529a27d9a716f9f
Staci Genesi <sgenesi@hotmail.com>
2021-01-02 18:25:57 -0700

    created modal for mispelled drink ingredient

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,12 +34,41 @@ var getDrinksByMainIngredient = function (ingredient) {
                     // document.getElementById("response-container").innerHTML = drink;
                 };
                 // document.getElementById("response-container").innerHTML = drinkArray;
-            })
+            }, error => {
+                var modal = document.getElementById("drinkModal");
+                modal.style.display = "block";
+            });
         }
         // if unsuccessful, open a modal
         else {
-            alert('Something went wrong!');
+            console.log("error");
+            // Get the modal
+            var modal = document.getElementById("drinkModal");
+            modal.style.display = "block";
+
+            // Get the <span> element that closes the modal
+            //var span = document.getElementsById("modal-close");
+
+            // When the user clicks on the button, open the modal
+            //btn.onclick = function () {
+            //    modal.style.display = "block";
+            //}
+
+            // When the user clicks on <span> (x), close the modal
+            //span.onclick = function () {
+            //    modal.style.display = "none";
+            //}
+
+            // When the user clicks anywhere outside of the modal, close it
+            //window.onclick = function (event) {
+            //    if (event.target == modal) {
+            //        modal.style.display = "none";
+            //   }
+            //}
         }
+    }, error => {
+        //pop modal saying the API is down
+        console.log("api is down");
     });
 };
 
