72cb14ee2c09c3ef386f25ab3eecae23741e9f10
Staci Genesi <sgenesi@hotmail.com>
2021-01-03 11:58:25 -0700

    updated modal to try to get it to close

--- assets/js/script.js
+++ assets/js/script.js
@@ -201,3 +201,5 @@ var btn = document.getElementById("search-recipes");
 btn.addEventListener("click", loadRecipes);
 var recipeContainerEl = document.getElementById("recipe-container");
 // recipeContainerEl.addEventListener("click", saveRecipe);
+
+
